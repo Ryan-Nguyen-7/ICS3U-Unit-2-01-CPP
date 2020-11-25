42cbcf17e76ccc11e458724720307e7c01837cc6
Ubuntu <ubuntu@ip-172-31-40-214.ec2.internal>
2020-11-25 22:29:38 +0000

    added circleAP.cpp program

---
+++ circleAP.cpp
@@ -0,0 +1,17 @@
+// Copyright (C) 2020 St. Mother Teresa HS All rights reserved
+//
+// created by Ryan Nguyen
+// Created on November 2020
+// This program calculates the area and perimeter of a circle
+//     with a radius of 15cm
+
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "If a circle has a radius of 15cm" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Area is " << (M_PI*pow(15, 2)) << "cm^2" << std::endl;
+    std::cout << "Perimeter is " << (2*M_PI*15) << "cm" << std::endl;
+}
